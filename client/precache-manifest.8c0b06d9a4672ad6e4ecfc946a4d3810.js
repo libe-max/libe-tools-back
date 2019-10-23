9cfa52e62b77df049ad69c65d961751f5d33d359
Maxime Fabas <m.fabas@liberation.fr>
2019-10-23 19:36:33 +0200

    🔥 production ready

---
+++ client/precache-manifest.8c0b06d9a4672ad6e4ecfc946a4d3810.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d54fec5917a32c5d2be2b50deb06b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804d2e3d9f3b61bc2ac8",
+    "url": "/static/js/2.109b5c74.chunk.js"
+  },
+  {
+    "revision": "7485a9340478cc368603",
+    "url": "/static/js/main.66bccde6.chunk.js"
+  },
+  {
+    "revision": "a83823e404e91fbc4624",
+    "url": "/static/js/runtime-main.c2cb764f.js"
+  },
+  {
+    "revision": "031d5a71679630c9908bd0966559b869",
+    "url": "/static/media/bg-image-placeholder.031d5a71.svg"
+  },
+  {
+    "revision": "e1f4a97b1a626abe2d9c6620f8868d4b",
+    "url": "/static/media/image-placeholder.e1f4a97b.svg"
+  },
+  {
+    "revision": "b83b9db2907a2b89b569f2dfed7045f5",
+    "url": "/static/media/libe-quote-sign.b83b9db2.png"
+  },
+  {
+    "revision": "d753541fb81b31e11307ad218febc948",
+    "url": "/static/media/no-display-bg-placeholder.d753541f.svg"
+  }
+]);
